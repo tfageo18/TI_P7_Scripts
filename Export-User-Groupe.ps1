f6be7eaddb6a987cb3aaf44f50b77071a07fe143
tfageo18 <thomas.fageol@gmail.com>
2022-08-01 19:36:04 +0200

    le script enfin la  base

--- Export-User-Groupe.ps1
+++ Export-User-Groupe.ps1
@@ -1,10 +1,42 @@
 <#
 
 Auteur    : Thomas FAGEOL
-Date      : 22/05/2022
+Date      : 01/08/2022
 Version   : 1.0
-RÈvisions : 
-  - 1.0 : CrÈation
-Description : Afficher/díexporter la liste des groupes AD díun utilisateur
+Description : Afficher/exporter la liste des groupes AD d'un utilisateur
 
 #>
+
+$osInfo = Get-CimInstance -ClassName Win32_OperatingSystem
+If ($osInfo.ProductType -eq 1) { $debug = 1}
+If ($osInfo.ProductType -eq 2) { $debug = 0}
+
+If ($debug -eq 0) { Import-Module ActiveDirectory }
+
+# Choix du groupe
+$ou = 'OU=Utilisateurs,OU=_AXEPLANE,DC=axeplane,DC=loc' # OU des groupes
+# Liste des droits d'acc√®s pour les fichiers
+If ($debug -eq 0) { 
+  $utilAD = @()
+  $utils  = Get-ADUser -SearchBase $ou -Filter * | Sort-Object | select Name 
+
+  ForEach ($util in $utils) { $utilAD += $util.Name }
+}
+
+# Export
+$question = Read-Host "Voulez-vous un export CSV (o/n) ?"
+
+If ($debug -eq 0) { 
+  For($i = 0; $i -lt $utilAD.count; $i++){
+    Write-Host "$($i): $($utilAD[$i])"
+  }
+  $nbutilAD = Read-Host "Choisir le numero de l'utilsateur"
+
+  $nomUtil  = $utilAD[$nbutilAD]
+
+  If ($question -eq 'n') {
+    (Get-ADuser -Identity $nomUtil -Properties memberof).memberof
+  }
+  If ($question -eq 'o') {
+  }
+}
